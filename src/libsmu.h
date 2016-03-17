0e9d8b0ebb061b12da5d0021a4ee461dfc3e4b39
Tim Harder <timothy.harder@analog.com>
2016-03-17 15:08:45 -0400

    src/libsmu.h: simplify debug macro so it works for VS

--- src/libsmu.h
+++ src/libsmu.h
@@ -16,9 +16,7 @@
 #define DEBUG_TEST 0
 #endif
 
-#define _debug(format, ...) \
-	do { if (DEBUG_TEST) fprintf(stderr, "DEBUG: %s:%d: " format "\n", __FUNCTION__, __LINE__, __VA_ARGS__); } while(0);
-#define debug(...) _debug(__VA_ARGS__, "")
+#define debug(...) do { if (DEBUG_TEST) fprintf(stderr, __VA_ARGS__); } while(0);
 
 #ifdef __GNUC__
 #define __packed __attribute__((packed))
