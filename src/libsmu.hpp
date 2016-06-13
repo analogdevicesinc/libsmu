4c91bd337bef6579d7693f1c858207f4a22db3fc
Tim Harder <timothy.harder@analog.com>
2016-06-13 19:19:24 -0400

    libsmu.hpp: merge libsmu.h data structures
    Trying to cut down on the complexity of the header layout.

--- src/libsmu.hpp
+++ src/libsmu.hpp
@@ -5,7 +5,8 @@
 //   Ian Daniher <itdaniher@gmail.com>
 
 #pragma once
-#include "libsmu.h"
+#include <cstdint>
+#include <cstddef>
 #include <string>
 #include <set>
 #include <memory>
@@ -23,11 +24,71 @@ using std::vector;
 #define M_PI (4.0*atan(1.0))
 #endif
 
+#ifdef DEBUG
+#define DEBUG_TEST 1
+#else
+#define DEBUG_TEST 0
+#endif
+
+#define smu_debug(...) do { if (DEBUG_TEST) fprintf(stderr, __VA_ARGS__); } while(0);
+
 class Device;
 class Signal;
 struct libusb_device;
 struct libusb_device_handle;
 struct libusb_context;
+
+typedef enum sl_type {
+	DEVICE_M1000 = 0x10000,
+	CHANNEL_SMU = 0x20000,
+	MODE_HIGH_Z = 0x40000,
+	MODE_SVMI,
+	MODE_SIMV,
+	SIGNAL = 0x80000,
+} sl_type;
+
+typedef struct sl_unit {
+	int8_t m;
+	int8_t kg;
+	int8_t s;
+	int8_t A;
+	int8_t K;
+	int8_t mol;
+	int8_t cd;
+} sl_unit;
+
+const sl_unit unit_V = { 2,  1, -3, -1,  0,  0,  0};
+const sl_unit unit_A = { 0,  0,  0,  1,  0,  0,  0};
+
+typedef struct sl_signal_info {
+	sl_type type;
+
+	const char* label;
+
+	/// Bitmask of modes for which this signal is enabled as input
+	uint32_t inputModes;
+
+	/// Bitmask of modes for which this signal is enabled as output
+	uint32_t outputModes;
+
+	sl_unit unit;
+	double min;
+	double max;
+	double resolution;
+} sl_signal_info;
+
+typedef struct sl_channel_info {
+	sl_type type;
+	const char* label;
+	size_t mode_count;
+	size_t signal_count;
+} sl_channel_info;
+
+typedef struct sl_device_info {
+	sl_type type;
+	const char* label;
+	size_t channel_count;
+} sl_device_info;
 
 class Session {
 public:
