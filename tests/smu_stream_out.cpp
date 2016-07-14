e74ed8c567a8e7f394601a4891ce4f56acb2ea50
Tim Harder <timothy.harder@analog.com>
2016-07-14 16:49:31 -0400

    cmake: import tests and add cmake framework

---
+++ tests/smu_stream_out.cpp
@@ -0,0 +1,62 @@
+// Simple test for reading data without dropping samples.
+//
+// When run the test should output summed sample values from all channels to
+// stdout in a continuous fashion. If any sample is dropped the program exits
+// with an error code of 1.
+
+#include <array>
+#include <iostream>
+#include <vector>
+#include <system_error>
+
+#include <libsmu/libsmu.hpp>
+
+using namespace smu;
+
+int main(int argc, char **argv)
+{
+	// Create session object and scan system for compatible devices then add
+	// them to the session. Note that this currently doesn't handle returned
+	// errors.
+	Session* session = new Session();
+	session->scan();
+	session->add_all();
+
+	// Grab the first device from the session (we're assuming one exists).
+	auto dev = *(session->m_devices.begin());
+
+	// Run session at the default device rate.
+	session->configure(dev->get_default_rate());
+
+	// Run session in continuous mode.
+	session->start(0);
+
+	// Data to be read from the device is formatted into a vector of four
+	// floats in an array, specifically in the format
+	// <Chan A voltage, Chan A current, Chan B coltage, Chan B current>.
+	std::vector<std::array<float, 4>> buf;
+	float v;
+
+	while (true) {
+		try {
+			// Read 1024 samples at a time from the device.
+			// Note that the timeout (3rd parameter to read() defaults to 0
+			// (nonblocking mode) so the number of samples returned won't
+			// necessarily be 1024.
+			dev->read(buf, 1024);
+		} catch (const std::system_error& e) {
+			// Exit on dropped samples.
+			fprintf(stderr, "sample(s) dropped!\n");
+			exit(1);
+		}
+
+		// Iterate over all returned samples, doesn't have to be 1024).
+		for (auto i: buf) {
+			// Do something random but (probably) fast enough so samples aren't
+			// dropped. This could depend on the terminal it gets run on due to
+			// the printf() if stdout isn't redirected.
+			v = i[0] + i[1] + i[2] + i[3];
+			printf("%f\n", v);
+		}
+	};
+}
