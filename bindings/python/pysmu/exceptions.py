9126ce5fe0f1a04fa5f912183308e216b453948e
Tim Harder <timothy.harder@analog.com>
2016-08-04 16:13:50 -0400

    pysmu: exceptions: always assign value to errcode attr

--- bindings/python/pysmu/exceptions.py
+++ bindings/python/pysmu/exceptions.py
@@ -7,8 +7,7 @@ class SessionError(Exception):
     def __init__(self, msg=None, errcode=None):
         if msg is not None:
             self.msg = msg
-        if errcode is not None:
-            self.errcode = abs(errcode)
+        self.errcode = abs(errcode) if errcode is not None else None
 
     def __str__(self):
         if self.msg is not None:
