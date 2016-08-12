b4e23d0340485f7f3ae36bd5a83502d8ccde578d
Tim Harder <timothy.harder@analog.com>
2016-08-12 16:49:09 -0400

    pysmu: setup: allow running tests via ./setup.py test
    Note that the tests are more system integration style tests so they
    cannot be parallelized and require some actions to be performed and
    status to be checked during different phases.

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -5,12 +5,14 @@ from io import open
 import glob
 import os
 import re
+import shlex
+import shutil
 import subprocess
 import sys
 
 from distutils.command.build_ext import build_ext as dst_build_ext
 from distutils.command.sdist import sdist as dst_sdist
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages, Extension, Command
 
 # top level bindings directory
 BINDINGS_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -128,6 +130,72 @@ class build_ext(dst_build_ext):
         return dst_build_ext.run(self)
 
 
+class PyTest(Command):
+    """Run tests using pytest against a built copy."""
+
+    user_options = [
+        ('pytest-args=', 'a', 'arguments to pass to py.test'),
+        ('coverage', 'c', 'generate coverage info'),
+        ('report=', 'r', 'generate and/or show a coverage report'),
+        ('match=', 'k', 'run only tests that match the provided expressions'),
+    ]
+
+    default_test_dir = os.path.join(BINDINGS_DIR, 'tests')
+
+    def initialize_options(self):
+        self.pytest_args = ''
+        self.coverage = False
+        self.match = None
+        self.report = None
+
+    def finalize_options(self):
+        self.test_args = ['-s', self.default_test_dir]
+        self.coverage = bool(self.coverage)
+        if self.match is not None:
+            self.test_args.extend(['-k', self.match])
+
+        if self.coverage:
+            try:
+                import pytest_cov
+                self.test_args.extend(['--cov', 'pysmu'])
+            except ImportError:
+                sys.stderr.write('error: install pytest-cov for coverage support\n')
+                sys.exit(1)
+
+            if self.report is None:
+                # disable coverage report output
+                self.test_args.extend(['--cov-report='])
+            else:
+                self.test_args.extend(['--cov-report', self.report])
+
+        # add custom pytest args
+        self.test_args.extend(shlex.split(self.pytest_args))
+
+    def run(self):
+        try:
+            import pytest
+        except ImportError:
+            sys.stderr.write('error: pytest is not installed\n')
+            sys.exit(1)
+
+        # build extensions and byte-compile python
+        build_ext = self.reinitialize_command('build_ext')
+        build_py = self.reinitialize_command('build_py')
+        build_ext.ensure_finalized()
+        build_py.ensure_finalized()
+        self.run_command('build_ext')
+        self.run_command('build_py')
+
+        # Change the current working directory to the builddir during testing
+        # so coverage paths are correct.
+        builddir = os.path.abspath(build_py.build_lib)
+        if self.coverage and os.path.exists(os.path.join(TOPDIR, '.coveragerc')):
+            shutil.copyfile(os.path.join(TOPDIR, '.coveragerc'),
+                            os.path.join(builddir, '.coveragerc'))
+        ret = subprocess.call([sys.executable, '-m', 'pytest'] + self.test_args, cwd=builddir)
+        sys.exit(ret)
+
+
 version = ''
 with open('pysmu/__init__.py', 'r') as fd:
     reg = re.compile(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]')
@@ -140,6 +208,11 @@ with open('pysmu/__init__.py', 'r') as fd:
 if not version:
     raise RuntimeError('Cannot find version information')
 
+
+test_requirements = ['pytest']
+if sys.hexversion < 0x03030000:
+        test_requirements.append('mock')
+
 setup(
     name='pysmu',
     version=version,
@@ -151,10 +224,11 @@ setup(
     packages=find_packages(),
     ext_modules=extensions,
     scripts=glob.glob('bin/*'),
-    test_suite='tests',
+    tests_require=test_requirements,
     cmdclass={
         'build_ext': build_ext,
         'sdist': sdist,
+        'test': PyTest,
     },
     classifiers=[
         'Intended Audience :: Developers',
