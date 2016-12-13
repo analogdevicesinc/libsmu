bf5ac1eaced9c14fc4ac826ccc3359aa77c7c2c6
Tim Harder <timothy.harder@analog.com>
2016-12-13 18:19:13 -0500

    pysmu/examples/multi-run-flush: iterate of current values as well for channel B
    In addition, iterate over a wider range of potential values between min
    and max values for voltage and current.

--- bindings/python/examples/multi-run-flush.py
+++ bindings/python/examples/multi-run-flush.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         dev.channels['A'].mode = Mode.SVMI
         dev.channels['B'].mode = Mode.SIMV
 
-    for x in range(10):
+    for x in range(21):
         # Flush the read/write queues for all the devices in the session.
         # Without doing this the values won't change between iterations due to
         # the queues being entirely filled with values from the initial
@@ -28,13 +28,14 @@ if __name__ == '__main__':
         session.flush()
 
         for dev in session.devices:
-            v = x % 6
+            v = x * 5/20.0
+            i = ((x * 4/20.0) / 10.0) - 0.2
             dev.channels['A'].constant(v)
-            dev.channels['B'].constant(.05)
+            dev.channels['B'].constant(i)
 
         session.run(10)
 
         for idx, dev in enumerate(session.devices):
-            print('dev: {}: chan A voltage: {}'.format(idx + 1, v))
+            print('dev: {}: chan A voltage: {}, chan B current: {}'.format(idx + 1, v, i))
             for x in dev.get_samples(10):
                 print("{: 6f} {: 6f} {: 6f} {: 6f}".format(x[0][0], x[0][1], x[1][0], x[1][1]))
