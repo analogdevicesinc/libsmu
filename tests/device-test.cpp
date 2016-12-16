1b834c1e9db85c00fd94d64a2f9a889feb368d73
Tim Harder <timothy.harder@analog.com>
2016-12-16 17:27:02 -0500

    tests: add initial device test stub

---
+++ tests/device-test.cpp
@@ -0,0 +1,28 @@
+// Tests for device functionality.
+
+#include <gtest/gtest.h>
+
+#include "fixtures.hpp"
+#include <libsmu/libsmu.hpp>
+
+using namespace smu;
+
+class DeviceTest : public SingleDeviceFixture {};
+
+
+TEST_F(DeviceTest, serial) {
+	ASSERT_NE(m_device->m_serial, "");
+}
+
+TEST_F(DeviceTest, fwver) {
+	ASSERT_NE(m_device->m_fwver, "");
+}
+
+TEST_F(DeviceTest, hwver) {
+	ASSERT_NE(m_device->m_hwver, "");
+}
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
