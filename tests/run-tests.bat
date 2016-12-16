f5e60c5e7e28127d2063daab476a7f96d258f5a0
Tim Harder <timothy.harder@analog.com>
2016-12-16 18:40:45 -0500

    tests: rename test files to be similarly prefixed instead of suffixed
    So with file sorting it's easy to see all of them on Windows at a glance
    in the unzipped archive.

--- tests/run-tests.bat
+++ tests/run-tests.bat
@@ -2,8 +2,8 @@ rem Run test suite on Windows.
 rem
 rem Executes all test applications within the current directory.
 rem
-rem Note that all test executables must be named "testname-test.exe" in order
+rem Note that all test executables must be named "test-testname.exe" in order
 rem to be run.
 
-for /r "." %%a in (*-test.exe) do start /b "" "%%~fa"
+for /r "." %%a in (test-*.exe) do start /b "" "%%~fa"
 pause
