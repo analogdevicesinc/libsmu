63e0607c0c3448469186a9edd0a35debb2823374
Tim Harder <timothy.harder@analog.com>
2016-12-17 02:06:34 -0500

    tests: drop batch file tests runner
    We'll use a test runner such as gtest-runner [1] instead.
    
    [1]: https://github.com/nholthaus/gtest-runner

--- tests/run-tests.bat
+++
@@ -1,9 +0,0 @@
-rem Run test suite on Windows.
-rem
-rem Executes all test applications within the current directory.
-rem
-rem Note that all test executables must be named "test-testname.exe" in order
-rem to be run.
-
-for /r "." %%a in (test-*.exe) do start /b "" "%%~fa"
-pause
