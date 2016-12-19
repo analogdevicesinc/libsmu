e629a353fd642d5133e6949bf4002ed67d3e53b8
Tim Harder <timothy.harder@analog.com>
2016-12-19 14:03:03 -0500

    pysmu/examples/get_samples.py: use contant voltage value based on device index

--- bindings/python/examples/get_samples.py
+++ bindings/python/examples/get_samples.py
@@ -16,11 +16,11 @@ if __name__ == '__main__':
     if not session.devices:
         sys.exit()
 
-    for dev in session.devices:
+    for idx, dev in enumerate(session.devices):
         # Set all devices in the session to use source voltage, measure current
-        # mode for channel A with a constant value of 4.
+        # mode for channel A with a constant value based on their index.
         dev.channels['A'].mode = Mode.SVMI
-        dev.channels['A'].constant(4)
+        dev.channels['A'].constant(idx % 6)
         # Set all devices in the session to use source current, measure voltage
         # mode for channel B with a constant value of 0.05.
         dev.channels['B'].mode = Mode.SIMV
