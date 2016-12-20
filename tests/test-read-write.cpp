1b630c727c918c4f083213eb70d4147e84aab3d1
Tim Harder <timothy.harder@analog.com>
2016-12-19 23:39:28 -0500

    tests/test-read-write: initial read/write test in continuous mode

--- tests/test-read-write.cpp
+++ tests/test-read-write.cpp
@@ -67,6 +67,58 @@ TEST_F(ReadWriteTest, non_continuous) {
 	}
 }
 
+TEST_F(ReadWriteTest, continuous) {
+	// Set device channels to source voltage and measure current.
+	m_dev->set_mode(0, SVMI);
+	m_dev->set_mode(1, SVMI);
+
+	std::vector<std::array<float, 4>> rxbuf;
+	std::vector<float> a_txbuf;
+	std::vector<float> b_txbuf;
+
+	// Run session in continuous mode.
+	m_session->start(0);
+
+	// Verify read/write data for 10 seconds.
+	unsigned voltage = 0;
+	uint64_t sample_count = 0;
+	auto clk_start = std::chrono::high_resolution_clock::now();
+	while (true) {
+		auto clk_end = std::chrono::high_resolution_clock::now();
+		auto clk_diff = std::chrono::duration_cast<std::chrono::seconds>(clk_end - clk_start);
+		if (clk_diff.count() > 10)
+			break;
+
+		// Refill outgoing data buffers.
+		refill_data(a_txbuf, 1000, voltage % 6);
+		refill_data(b_txbuf, 1000, voltage % 6);
+
+		try {
+			// Write iterating voltage values to both channels.
+			m_dev->write(a_txbuf, 0);
+			m_dev->write(b_txbuf, 1);
+
+			// Read incoming samples in a non-blocking fashion.
+			m_dev->read(rxbuf, 1000);
+		} catch (const std::runtime_error& e) {
+			// ignore sample drops
+		}
+
+		// Validate received values.
+		for (unsigned i = 0; i < rxbuf.size(); i++) {
+			sample_count++;
+			EXPECT_EQ(voltage % 6, std::fabs(std::round(rxbuf[i][0]))) << "value: " << rxbuf[i][0] << " ,failed at sample: " << sample_count;
+			EXPECT_EQ(voltage % 6, std::fabs(std::round(rxbuf[i][2]))) << "value: " << rxbuf[i][2] << " ,failed at sample: " << sample_count;
+		}
+
+		if (sample_count && sample_count % 1000 == 0)
+			voltage++;
+	}
+
+	// Verify we're running at 100+ ksps.
+	EXPECT_GE(sample_count, 100000*10);
+}
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
