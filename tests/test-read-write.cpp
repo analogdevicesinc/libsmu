c56c5c0e2d71110778181a15f17d7b62d1e248cd
Tim Harder <timothy.harder@analog.com>
2016-12-19 23:38:12 -0500

    tests/test-read-write: run read/write captures for at least ten seconds

--- tests/test-read-write.cpp
+++ tests/test-read-write.cpp
@@ -4,6 +4,7 @@
 
 #include <cmath>
 #include <array>
+#include <chrono>
 #include <thread>
 #include <vector>
 
@@ -14,6 +15,14 @@ using namespace smu;
 
 class ReadWriteTest : public SingleDeviceFixture {};
 
+// refill Tx buffers with data
+static void refill_data(std::vector<float>& buf, unsigned size, int voltage) {
+	buf.clear();
+	for (unsigned i = 0; i < size; i++) {
+		buf.push_back(voltage);
+	}
+}
+
 TEST_F(ReadWriteTest, non_continuous) {
 	// Set device channels to source voltage and measure current.
 	m_dev->set_mode(0, SVMI);
@@ -23,18 +32,19 @@ TEST_F(ReadWriteTest, non_continuous) {
 	std::vector<float> a_txbuf;
 	std::vector<float> b_txbuf;
 
-	// refill Tx buffers with data
-	auto refill_data = [=](std::vector<float>& buf, unsigned size, int voltage) {
-		buf.clear();
-		for (unsigned i = 0; i < size; i++) {
-			buf.push_back(voltage);
-		}
-	};
+	// Verify read/write data for 10 seconds.
+	unsigned voltage = 0;
+	uint64_t sample_count = 0;
+	auto clk_start = std::chrono::high_resolution_clock::now();
+	while (true) {
+		auto clk_end = std::chrono::high_resolution_clock::now();
+		auto clk_diff = std::chrono::duration_cast<std::chrono::seconds>(clk_end - clk_start);
+		if (clk_diff.count() > 10)
+			break;
 
-	for (auto v = 0; v <= 5; v++) {
 		// Refill outgoing data buffers.
-		refill_data(a_txbuf, 1024, v);
-		refill_data(b_txbuf, 1024, v);
+		refill_data(a_txbuf, 1024, voltage % 6);
+		refill_data(b_txbuf, 1024, voltage % 6);
 
 		// Write iterating voltage values to both channels.
 		m_dev->write(a_txbuf, 0);
@@ -48,10 +58,12 @@ TEST_F(ReadWriteTest, non_continuous) {
 
 		// Validate received values.
 		EXPECT_EQ(rxbuf.size(), 1024);
-		for (auto x: rxbuf) {
-			EXPECT_EQ(v, fabs(round(x[0])));
-			EXPECT_EQ(v, fabs(round(x[2])));
+		for (unsigned i = 0; i < rxbuf.size(); i++) {
+			sample_count++;
+			EXPECT_EQ(voltage % 6, std::fabs(std::round(rxbuf[i][0]))) << "failed at sample: " << sample_count;
+			EXPECT_EQ(voltage % 6, std::fabs(std::round(rxbuf[i][2]))) << "failed at sample: " << sample_count;
 		}
+		voltage++;
 	}
 }
 
