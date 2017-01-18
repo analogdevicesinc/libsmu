3b9aafe70637bb71d3b24110cec127ec6f661abe
Tim Harder <timothy.harder@analog.com>
2017-01-18 18:52:22 -0500

    pysmu/tests/test_read: add initial test_read_continuous_sample_rates check

--- bindings/python/tests/test_read.py
+++ bindings/python/tests/test_read.py
@@ -106,3 +106,79 @@ def test_read_continuous_timeout(session, device):
 
     # Which should be long enough to get all 1000 samples.
     assert len(samples) == 1000
+
+def test_read_continuous_sample_rates(session, device):
+    """Verify streaming HI-Z data values and speed from 100 kSPS to 10 kSPS every ~5k SPS."""
+    sample_count = 0
+
+    failure_vals = []
+    failure_samples = []
+    sys.stdout.write('\n')
+
+    for rate in range(100, 5, -5):
+        sample_count = long(0)
+        failure = False
+        session.sample_rate = rate * 1000
+        sample_rate = session.sample_rate
+
+        # Make sure the session got configured properly.
+        if sample_rate < 0:
+            print("failed to configure session: {}".format(sample_rate))
+            continue
+
+        # Verify we're within the minimum configurable range from the specified target.
+        assert abs((rate * 1000) - sample_rate) <= 256
+        sys.stdout.write("running test at {} SPS: ".format(sample_rate))
+
+        start = time.time()
+        session.start(0)
+
+        while True:
+            end = time.time()
+            clk_diff = end - start
+            # Run each session for a minute.
+            if clk_diff > 60:
+                break
+
+            # Grab 1000 samples in a non-blocking fashion in HI-Z mode.
+            samples = device.read(1000)
+
+            # Which all should be near 0.
+            for sample in samples:
+                sample_count += 1
+                for x in chain.from_iterable(sample):
+                    if abs(round(x)) != 0:
+                        failure = True
+                        failure_vals.append(x)
+                        failure_samples.append(sample_count)
+
+                # show output progress per second
+                if sample_count % sample_rate == 0:
+                    if failure:
+                        failure = False
+                        sys.stdout.write('#')
+                    else:
+                        sys.stdout.write('*')
+                    sys.stdout.flush()
+
+        sys.stdout.write('\n')
+
+        # check if bad sample values were received and display them if any exist
+        if failure_samples:
+            print("{} bad sample(s):".format(len(failure_samples)))
+            for i, x in enumerate(failure_samples):
+                print("sample: {}, expected: 0, received: {}".format(x, failure_vals[i]))
+
+        failure_samples = []
+        failure_vals = []
+
+        samples_per_second = int(round(sample_count / clk_diff))
+        # Verify we're running within 250 SPS of the configured sample rate.
+        sample_rate_diff = int(samples_per_second - sample_rate)
+        assert abs(sample_rate_diff) <= 250
+        print("received {} samples in {} seconds: ~{} SPS ({} SPS difference)".format(
+            sample_count, int(round(clk_diff)), samples_per_second, sample_rate_diff))
+
+        # Stop the session.
+        session.cancel()
+        session.end()
