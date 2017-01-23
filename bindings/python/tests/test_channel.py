27b989bf871d76dab6a3d0cb69e4494955f82478
Tim Harder <radhermit@gmail.com>
2017-01-23 17:42:03 -0500

    pysmu/tests/test_channel: add channel fixtures to simplify calls

--- bindings/python/tests/test_channel.py
+++ bindings/python/tests/test_channel.py
@@ -17,57 +17,67 @@ def device(session):
     return session.devices[0]
 
 
-def test_mode(device):
+@pytest.fixture(scope='function')
+def chan_a(device):
+    return device.channels['A']
+
+
+@pytest.fixture(scope='function')
+def chan_b(device):
+    return device.channels['B']
+
+
+def test_mode(chan_a, chan_b):
     # channels start in HI_Z mode by default
-    assert device.channels['A'].mode == device.channels['B'].mode == Mode.HI_Z
+    assert chan_a.mode == chan_b.mode == Mode.HI_Z
 
     # invalid mode assignment raises ValueError
     with pytest.raises(ValueError):
-        device.channels['A'].mode = 4
+        chan_a.mode = 4
 
     # raw values can't be used for assignment, enum aliases must be used
     with pytest.raises(ValueError):
-        device.channels['A'].mode = 1
+        chan_a.mode = 1
 
-    device.channels['A'].mode = device.channels['B'].mode = Mode.SVMI
-    assert device.channels['A'].mode == device.channels['B'].mode == Mode.SVMI
+    chan_a.mode = chan_b.mode = Mode.SVMI
+    assert chan_a.mode == chan_b.mode == Mode.SVMI
 
 
-def test_channel_read(session, device):
+def test_channel_read(session, chan_a):
     session.run(1000)
-    samples = device.channels['A'].read(1000, -1)
+    samples = chan_a.read(1000, -1)
     assert len(samples) == 1000
     assert len(samples[0]) == 2
 
 
-def test_channel_write(device):
+def test_channel_write(chan_a, chan_b):
     pass
 
 
-def test_get_samples(device):
-    samples = device.channels['A'].get_samples(1000)
+def test_get_samples(chan_a, chan_b):
+    samples = chan_a.get_samples(1000)
     assert len(samples) == 1000
     assert len(samples[0]) == 2
 
 
-def test_arbitrary(device):
+def test_arbitrary(chan_a, chan_b):
     pass
 
 
-def test_constant(session, device):
-    device.channels['A'].mode = Mode.SVMI
-    device.channels['A'].constant(2)
-    device.channels['B'].mode = Mode.SVMI
-    device.channels['B'].constant(4)
+def test_constant(chan_a, chan_b):
+    chan_a.mode = Mode.SVMI
+    chan_a.constant(2)
+    chan_b.mode = Mode.SVMI
+    chan_b.constant(4)
 
     # verify sample values are near 2 for channel A
-    samples = device.channels['A'].get_samples(1000)
+    samples = chan_a.get_samples(1000)
     assert len(samples) == 1000
     for x in samples:
         assert abs(round(x[0])) == 2
 
     # verify sample values are near 4 for channel B
-    samples = device.channels['B'].get_samples(1000)
+    samples = chan_b.get_samples(1000)
     assert len(samples) == 1000
     for x in samples:
         assert abs(round(x[0])) == 4
