412bd87b37e2328ddc6863f755d9eba4ba2aaca3
Tim Harder <radhermit@gmail.com>
2017-02-14 01:18:20 -0500

    pysmu/tests/misc: add support for flashing the latest dev firmware

--- bindings/python/tests/misc.py
+++ bindings/python/tests/misc.py
@@ -8,6 +8,7 @@ except ImportError:
 
 OLD_FW_URL = 'https://github.com/analogdevicesinc/m1k-fw/releases/download/v2.02/m1000.bin'
 NEW_FW_URL = 'https://github.com/analogdevicesinc/m1k-fw/releases/download/v2.06/m1000.bin'
+DEV_FW_URL = 'https://github.com/analogdevicesinc/m1k-fw/raw/master/m1000.bin'
 
 # input = raw_input in py3, copy this for py2
 if sys.hexversion < 0x03000000:
@@ -23,5 +24,7 @@ def prompt(s):
 # fetch old/new firmware files from github
 OLD_FW = tempfile.NamedTemporaryFile().name
 NEW_FW = tempfile.NamedTemporaryFile().name
+DEV_FW = tempfile.NamedTemporaryFile().name
 urlretrieve(OLD_FW_URL, OLD_FW)
 urlretrieve(NEW_FW_URL, NEW_FW)
+urlretrieve(DEV_FW_URL, DEV_FW)
