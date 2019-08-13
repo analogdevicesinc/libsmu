18dd42e698afad03fd7b60d4a7ced277f23908fd
Cristi Iacob <cristian.iacob@analog.com>
2019-08-13 20:02:13 +0300

    Modified hotplug example.
    Signed-off-by: Cristi Iacob <cristian.iacob@analog.com>

--- examples/hotplug.cpp
+++ examples/hotplug.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <iostream>
 #include <thread>
+#include <algorithm>
 
 #include <libsmu/libsmu.hpp>
 
@@ -17,28 +18,52 @@ int main(int argc, char **argv)
 	// Create session object and add all compatible devices them to the
 	// session. Note that this currently doesn't handle returned errors.
 	Session* session = new Session();
-	session->add_all();
+	std::vector < Device* > last_devices;
 
-	// Register hotplug detach callback handler.
-	session->hotplug_detach([=](Device* device, void* data){
-		session->cancel();
-		if (!session->remove(device, true)) {
-			printf("device detached: %s: serial %s: fw %s: hw %s\n",
-				device->info()->label, device->m_serial.c_str(),
-				device->m_fwver.c_str(), device->m_hwver.c_str());
+	while (true) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+		session->scan();
+
+		std::vector < Device* > available_devices = session->m_available_devices;
+
+		//check if there is any disconnected device
+		for (auto other_device : last_devices) {
+			bool found = 0;
+
+			for (auto device : available_devices) {
+				if (!device->m_serial.compare(other_device->m_serial)) {
+					found = 1;
+					break;
+				}
+			}
+
+			if (!found) {
+				cout << "Device detached!\n";
+				last_devices.erase(std::remove(last_devices.begin(), last_devices.end(), other_device), last_devices.end());
+			}
 		}
-	});
 
-	// Register hotplug attach callback handler.
-	session->hotplug_attach([=](Device* device, void* data){
-		if (!session->add(device)) {
-			printf("device attached: %s: serial %s: fw %s: hw %s\n",
-				device->info()->label, device->m_serial.c_str(),
-				device->m_fwver.c_str(), device->m_hwver.c_str());
+		//check if there is any new connected device
+		for (auto device : available_devices) {
+			bool found = 0;
+
+			for (auto other_device : last_devices) {
+				if (!device->m_serial.compare(other_device->m_serial)) {
+					found = 1;
+					break;
+				}
+			}
+
+			if (!found) {
+				cout << "Device attached!\n";
+			}
+			else {
+				available_devices.erase(std::remove(available_devices.begin(), available_devices.end(), device), available_devices.end());
+				delete device;
+			}
 		}
-	});
 
-	cout << "waiting for hotplug events..." << endl;
-	while (true)
-		std::this_thread::sleep_for(std::chrono::seconds(1));
+		last_devices.insert(last_devices.end(), available_devices.begin(), available_devices.end());
+		cout << "Number of available devices: " << last_devices.size() << '\n';
+	}
 }
