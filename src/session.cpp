analogdevicesinc/libsmu

commit 0548b8505df4a9e121e3f6a1a529fe0ea9fe0f49

Solved unplug deadlock bug.
Sometimes when unplugging a device from a multi-device session a deadlock occurred.
This was happening because the m_state was locked by the samples transmission responsible thread.
Now we unlock that mutex because we are not anymore interested in the generated samples, because we have just closed the session.

Signed-off-by: Cristi Iacob <cristian.iacob@analog.com>

src/session.cpp
@@ -82,6 +82,12 @@ Session::~Session()
 	libusb_exit(m_usb_ctx);
 }
 
+void Session::set_off(Device* dev)
+{
+	M1000_Device *m_dev = dynamic_cast<M1000_Device*>(dev);
+	m_dev->m_state.unlock();
+}
+
 void Session::attached(libusb_device *usb_dev)
 {
 	if (!m_hotplug_attach_callbacks.empty()) {
@@ -560,10 +566,8 @@ int Session::run(uint64_t samples)
 int Session::end()
 {
 	int ret = 0;
-
 	// cancel continuous sessions before ending them
-	if (m_continuous) {
-		cancel();
+	if (m_continuous && m_cancellation) {
 		m_continuous = false;
 	}
 
@@ -639,10 +643,11 @@ int Session::cancel()
 
 	m_cancellation = LIBUSB_TRANSFER_CANCELLED;
 	for (Device* dev: m_devices) {
+		set_off(dev);
 		ret = dev->cancel();
+
 		if (ret)
 			break;
-		//delete dev;
 	}
 	return ret;
 }

include/libsmu/libsmu.hpp
@@ -111,6 +111,8 @@ namespace smu {
 		Session();
 		~Session();
 
+		void set_off(Device*);
+
 		/// @brief Devices that are present on the system.
 		/// Note that these devices consist of all supported devices currently
 		/// recognized on the system; however, the devices aren't necessarily

src/device_m1000.cpp
@@ -85,6 +85,7 @@ M1000_Device::~M1000_Device()
 	// free USB transfers
 	m_in_transfers.clear();
 	m_out_transfers.clear();
+	unlock();
 }
 
 int M1000_Device::get_default_rate()
@@ -980,8 +981,10 @@ int M1000_Device::run(uint64_t samples)
 			cv.wait(lk, [&buf,&stop]{ return (buf.size() || stop < 0); });
 
 			// signaled to exit
-			if (stop < 0)
+			if (stop < 0) {
+				lk.unlock();
 				return;
+			}
 			stop = 0;
 start:
 			it = buf.begin();
@@ -1031,7 +1034,7 @@ end:
 				m_out_samples_cv[ch_i].notify_one();
 			}
 		}
-	}
+    }
 
 	return 0;
 }
@@ -1039,13 +1042,14 @@ end:
 int M1000_Device::cancel()
 {
     lock();
+
 	int ret_in = m_in_transfers.cancel();
 	int ret_out = m_out_transfers.cancel();
-    if ((ret_in != ret_out) || (ret_in != 0) || (ret_out != 0)){
+	if ((ret_in != ret_out) || (ret_in != 0) || (ret_out != 0)){
         unlock();
 		return -1;
-    }
-    unlock();
+	}
+	unlock();
 	return 0;
 }
 

commit facaf4a6a98e3628ef7b8ce8787e0e1818e54844

session.cpp: fixed cancellation bug.
The cancel() call is needed in order to cancel the current transfers from each connected device before turning them off. This prevents libsmu from getting into a deadlock because of the m_state mutex locked in the run() method.
The m_cancellation flag must be removed from the if-statement because it is only set in the device->cancel() method, preventing the program from entering this if statement.

This bug was introduced in 0548b85. The introduced bug has hidden by Pixelpulse, which I have used to test that modification. In Pixelpulse, session->close() was called before session->end(), solving the above mentioned threading issue.

Signed-off-by: Cristi Iacob <cristian.iacob@analog.com>

src/session.cpp
@@ -579,7 +579,8 @@ int Session::end()
 {
 	int ret = 0;
 	// cancel continuous sessions before ending them
-	if (m_continuous && m_cancellation) {
+	if (m_continuous) {
+		cancel();
 		m_continuous = false;
 	}
 