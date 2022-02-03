73469760fa573124ac8c9e7b583f68e67c4d71ef
AlexandraTrifan <Alexandra.Trifan@analog.com>
2022-02-03 18:04:19 +0200

    Linux Builds: Drop the Python debian package builds.
    Signed-off-by: AlexandraTrifan <Alexandra.Trifan@analog.com>

--- CI/travis/install_linux.sh
+++ CI/travis/install_linux.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 
 if [[ ${OS_TYPE} == "doxygen" ]]; then make && sudo make install ; fi
-
-if [[ ${OS_TYPE} == "doxygen" ]]; then
-	cd "${BUILD_SOURCESDIRECTORY}"/bindings/python
-	python --version
-	python -c "import struct; print(struct.calcsize('P') * 8)"
-	python setup.py build_ext -L "${BUILD_SOURCESDIRECTORY}"/build/src
-	python setup.py build
-	python -c "from Cython.Build import cythonize"
-	python setup.py bdist_wheel --skip-build
-	sudo sed -i 's/my\ \$ignore_missing_info\ =\ 0;/my\ \$ignore_missing_info\ =\ 1;/' /usr/bin/dpkg-shlibdeps
-	python setup.py --command-packages=stdeb.command sdist_dsc
-	cd "$(find . -type d -name "debian" | head -n 1)"/..
-	debuild -us -uc
-	cp ${BUILD_SOURCESDIRECTORY}/bindings/python/deb_dist/*.deb ${BUILD_SOURCESDIRECTORY}/bindings/python
-fi
