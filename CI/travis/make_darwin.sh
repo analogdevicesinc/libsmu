98362393680bff014a267259e13456dc41c18a8e
AlexandraTrifan <Alexandra.Trifan@analog.com>
2022-02-03 17:33:47 +0200

    MacOS builds: Clean up build scripts and drop Python bindings builds.
    Signed-off-by: Alexandra Trifan <Alexandra.Trifan@analog.com>

--- CI/travis/make_darwin.sh
+++ CI/travis/make_darwin.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
+OS_VERSION=$( /usr/libexec/PlistBuddy -c "Print:ProductVersion" /System/Library/CoreServices/SystemVersion.plist )
+echo $OS_VERSION
 mkdir -p build && cd build
 
 # Build tar.gz for OSX (contains libraries and smu)
-cmake -DENABLE_PACKAGING=ON -DBUILD_PYTHON=OFF ..; make package
-
-# Build pkg for OSX (contains libsmu.framework - only base library)
-cmake -DOSX_PACKAGE=ON -DENABLE_PACKAGING=OFF -DUSE_PYTHON2=OFF ..
-make && sudo make install
-
-cd "${BUILD_SOURCESDIRECTORY}"/bindings/python
-python3 --version
-python3 -c "import struct; print(struct.calcsize('P') * 8)"
-python3 setup.py build_ext -L "${BUILD_SOURCESDIRECTORY}"/build/src
-python3 setup.py build
-python3 setup.py bdist_wheel --skip-build
+cmake -DBUILD_PYTHON=ON -DOSX_PACKAGE=ON -DCMAKE_OSX_DEPLOYMENT_TARGET=$OS_VERSION ..; make && sudo make install
