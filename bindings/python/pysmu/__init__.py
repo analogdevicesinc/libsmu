3012806e56f4d6c0a9c66fd4eeb85d54d5c17ecb
AlexandraTrifan <Alexandra.Trifan@analog.com>
2022-02-03 16:51:00 +0200

    pysmu/__init__.py: After python 3.7 the module is no longer discovered correctly.
    The setup.py file previously included this code snippet programatically
    at build time.
    
    Signed-off-by: Alexandra Trifan <Alexandra.Trifan@analog.com>

--- bindings/python/pysmu/__init__.py
+++ bindings/python/pysmu/__init__.py
@@ -3,5 +3,15 @@
 
 __version__ = '1.0.3'
 
+import os
+
+#Import DLLs for Python versions >= 3.8
+for path in os.environ.get("PATH", "").split(os.pathsep):
+	if path and os.path.isabs(path):
+		try:
+			os.add_dll_directory(path)
+		except (OSError, AttributeError):
+			continue
+
 from .libsmu import *
 from .exceptions import *
